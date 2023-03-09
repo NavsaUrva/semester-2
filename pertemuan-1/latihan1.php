551e38773d1132d24adf766fd375e3454cb42cfa
NavsaUrva <nvsaurva@gmail.com~>
2023-03-09 21:22:21 +0700

    first commit

---
+++ pertemuan-1/latihan1.php
@@ -0,0 +1,11 @@
+<?php
+
+//ini contoh deklarasi variable
+echo "Hello World";
+$nama = 'Navsa' ;
+$Umur = 18 ;
+$berat = 50 ;
+
+//ini contoh pemanggilan atau output variable
+echo '<br>Nama ' .$nama
+?>
